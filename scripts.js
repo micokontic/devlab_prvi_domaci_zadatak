b0e4525cb4f73a63875cb6659876b19a69e3516d
Mico Kontic <micokontic@gmail.com>
2020-11-03 20:16:42 +0100

    merged two halfs

---
+++ scripts.js
@@ -0,0 +1,33 @@
+function openPortfolio(cityName) {
+    var i;
+    var x = document.getElementsByClassName("portfolio");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(cityName).style.display = "block";
+  }
+
+  
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  // Get the container element
+var btnContainer = document.getElementById("bar");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("bar-item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
